cfbe26265c08c2c090686effbf0eb925ea4dc562
blacktwin <blacktwin@users.noreply.github.com>
2017-10-20 14:55:38 -0400

    plexapi and ssl update
    #12

---
+++ killstream/kill_plex_streams.py
@@ -0,0 +1,27 @@
+"""
+Kill all streams
+"""
+
+import requests
+from plexapi.server import PlexServer
+
+## EDIT THESE SETTINGS ##
+PLEX_TOKEN = 'xxxxx'
+PLEX_URL = 'http://localhost:32400'
+
+MESSAGE = 'Because....'
+ignore_lst = ('')
+##/EDIT THESE SETTINGS ##
+
+sess = requests.Session()
+sess.verify = False
+plex = PlexServer(PLEX_URL, PLEX_TOKEN, session=sess)
+
+def kill_session():
+    for session in plex.sessions():
+        user = session.username[0]
+        title = (session.grandparentTitle + ' - ' if session.type == 'episode' else '') + session.title
+        print("Killing {}'s stream of {} for {}".format(user, title, MESSAGE))
+        session.stop(reason=MESSAGE)
+
+kill_session()
