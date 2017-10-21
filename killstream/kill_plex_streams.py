17b2180a09e34c69c67c508d7a3b7a25cce27027
blacktwin <blacktwin@users.noreply.github.com>
2017-10-21 14:42:58 -0400

    added ignore

--- killstream/kill_plex_streams.py
+++ killstream/kill_plex_streams.py
@@ -20,8 +20,9 @@ plex = PlexServer(PLEX_URL, PLEX_TOKEN, session=sess)
 def kill_session():
     for session in plex.sessions():
         user = session.username[0]
-        title = (session.grandparentTitle + ' - ' if session.type == 'episode' else '') + session.title
-        print("Killing {}'s stream of {} for {}".format(user, title, MESSAGE))
-        session.stop(reason=MESSAGE)
+        if user not in ignore_lst:
+            title = (session.grandparentTitle + ' - ' if session.type == 'episode' else '') + session.title
+            print("Killing {}'s stream of {} for {}".format(user, title, MESSAGE))
+            session.stop(reason=MESSAGE)
 
 kill_session()
