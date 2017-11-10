55f4764533fa9431d9bd2b8f24967b028d210190
blacktwin <blacktwin@users.noreply.github.com>
2017-11-10 08:14:27 -0500

    adding print
    adding prints to identify what sub script is doing.

--- killstream/wait_kill_pause_notify_sub.py
+++ killstream/wait_kill_pause_notify_sub.py
@@ -25,12 +25,15 @@ interval = int(sys.argv[3])
 x = 0
 
 try:
+    print('Executing sub script.')
     while x < timeout and x is not None:
         sleep(x)
         if kill_stream(check_session(sessionKey), interval, timeout) is not None:
             x += kill_stream(check_session(sessionKey), interval, timeout)
         else:
+            print('Exiting sub script.')
             exit(0)
+    print('Sub script initiating kill.')
     kill_stream(check_session(sessionKey), timeout, timeout)
 
 except Exception as e:
