793092ddc81e6b2f6f5aaef8a1a62cdece6e9da7
Blacktwin <blacktwin@users.noreply.github.com>
2018-08-23 06:29:13 -0400

    CONFIG and SSL

--- utility/plex_api_parental_control.py
+++ utility/plex_api_parental_control.py
@@ -37,18 +37,33 @@ Usage:
 
 
 import argparse
+import requests
 from time import sleep
-from plexapi.server import PlexServer
+from plexapi.server import PlexServer, CONFIG
 
+MESSAGE = "GET TO BED!"
 
-PLEX_URL = 'http://localhost:32400'
-PLEX_TOKEN = 'xxxxxx'
-plex = PlexServer(PLEX_URL, PLEX_TOKEN)
+PLEX_URL = ''
+PLEX_TOKEN = ''
+PLEX_URL = CONFIG.data['auth'].get('server_baseurl', PLEX_URL)
+PLEX_TOKEN = CONFIG.data['auth'].get('server_token', PLEX_TOKEN)
+
+sess = requests.Session()
+# Ignore verifying the SSL certificate
+sess.verify = False  # '/path/to/certfile'
+# If verify is set to a path to a directory,
+# the directory must have been processed using the c_rehash utility supplied
+# with OpenSSL.
+if sess.verify is False:
+    # Disable the warning that the request is insecure, we know that...
+    import urllib3
+
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+plex = PlexServer(PLEX_URL, PLEX_TOKEN, session=sess)
 
 user_lst = [x.title for x in plex.myPlexAccount().users()]
 sections_lst = [x.title for x in plex.library.sections()]
-
-MESSAGE = "GET TO BED!"
 
 
 def share(user, libraries):
